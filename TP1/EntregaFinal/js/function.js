30d6d6138437443566af49048d6b217480af4369
elmuddy <fn.rosenfeld@gmail.com>
2017-08-24 17:25:04 -0300

    TPE 1 - Carga de imagen funcionando
    Jquery , vos y tu cambio de version
    $(window).load(function() { ... });
    To
    $(window).on('load', function() { ... });

--- TP1/EntregaFinal/js/function.js
+++ TP1/EntregaFinal/js/function.js
@@ -1,116 +1,161 @@
 
-  var ctx = document.getElementById("canvas").getContext("2d");
-  var width = 400;
-  var height =300;
+$(function() {
+    $('#file-input').change(function(e) {
+        var file = e.target.files[0],
+            imageType = /image.*/;
 
-var image1 = new Image();
-image1.src = "imagen1.jpg"; // asigna la direccion de donde cargar la imagen
+        if (!file.type.match(imageType))
+            return;
 
-image1.onload = function(){  // realiza la funcion despues de que cargo la imagen y con esto evita error
-  myDrawImageMethod(this);
-}
+        var reader = new FileReader();
+        reader.onload = fileOnload;
+        reader.readAsDataURL(file);
 
-function myDrawImageMethod(image){
-  ctx.drawImage(image,100,100,this.width,this.height);
-  imageData =  ctx.getImageData(50,50, this.width,this.height);
-  imageData2 = ctx.getImageData(50,50, this.width,this.height);
-  imageData3 = ctx.getImageData(50,50, this.width,this.height);
-  imageData4 = ctx.getImageData(50,50, this.width,this.height);
+    });
 
- for (x=0; x<width; x++){
-    for (y=0; y<height; y++){
-      // Negativo
-      setPixel(imageData, x, y, 255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y), 255);
-      // Gray Scale
-      prom = (getRed(imageData2,x,y) + getGreen(imageData2,x,y) + getBlue(imageData2,x,y))/3;
-      setPixel(imageData2, x, y, prom ,prom ,prom , 255);
+    function fileOnload(e) {
+        var $img = $('<img>', { src: e.target.result });
+        var canvas = $('#canvas')[0];
+        var context = canvas.getContext('2d');
 
-      // Sepia
-      red = getRed(imageData3,x,y);
-      green = getGreen(imageData3,x,y);
-      blue = getBlue(imageData3,x,y);
-
-      sepiaR = Math.floor(0.393*red + 0.769*green+ 0.189*blue);
-      sepiaG = Math.floor(0.349*red + 0.686*green + 0.168*blue);
-      sepiaB = Math.floor(0.272*red + 0.534*green + 0.131*blue);
-      setPixel(imageData3, x, y, sepiaR,sepiaG,sepiaB, 255);
-
-      // Binarizacion
-      prom2 = (getRed(imageData4,x,y) + getGreen(imageData4,x,y) + getBlue(imageData4,x,y))/3;
-      if (prom2 < 128){// el 128 deberia se una variable editable por el usuario el cual maneje el rango de cambio
-        prom2 = 0;
-      }
-      else {
-        prom2 = 255;
-      }
-      setPixel(imageData4, x, y, prom2 ,prom2 ,prom2 , 255);
+        $img.on('load', function() { // Te odio a vos y a esta linea que cambio en jquery-3
+            context.drawImage(this, 0, 0);
+        });
     }
-  }
-/*
-    // Brillo
-    red = getRed(imageData3,x,y);
-    green = getGreen(imageData3,x,y);
-    blue = getBlue(imageData3,x,y);
+});
 
-    var  brillo = 40;
-      red+= brillo;
-      green+= brillo;
-      blue+= brillo;
-      if  ((red > 255) or (red < 0)){
-        if (red >255){
-          red=255;
-        }
-        else {
-          red=0;
-        }
-      }
-      if  ((green > 255) or (green < 0)){
-        if (green >255){
-          green=255;
-        }
-        else {
-          green=0;
-        }
-      }
-
-      if  ((blue > 255) or (blue < 0)){
-        if (blue >255){
-          blue=255;
-        }
-        else {
-          blue=0;
-        }
-      }
-*/
-
-  ctx.putImageData(imageData,500, 50);
-  ctx.putImageData(imageData2,50, 370);
-  ctx.putImageData(imageData4,500, 370);
-
-}
+	  var ctx = document.getElementById("canvas").getContext("2d");
+	  var width = 500;
+	  var height =300;
+		var ctx2 = document.getElementById("canvas2").getContext("2d");
 
 
-// Get de los colores
-function getRed(imagedata, x, y){
-  index = (x + y * imageData.width)*4;
-  return imageData.data[index+0];
-}
+	var image1 = new Image();
+	image1.src = "imagen1.jpg"; // asigna la direccion de donde cargar la imagen
 
-function getGreen(imagedata, x, y){
-  index = (x + y * imageData.width)*4;
-  return imageData.data[index+1];
-}
+	image1.onload = function(){  // realiza la funcion despues de que cargo la imagen y con esto evita error
+	  myDrawImageMethod(this);
+	}
 
-function getBlue(imagedata, x, y){
-  index = (x + y * imageData.width)*4;
-  return imageData.data[index+2];
-}
+		function myDrawImageMethod(image){
+	  ctx.drawImage(image,0,0 ,canvas.width,canvas.height);
+	  imageData =  ctx.getImageData(0,0, canvas.width,canvas.width);
+
+		// Aca podria poner un case que me de la opcion de que filtro ponerle
+
+			function realizar_Negativo(){
+			 for (x=0; x<width; x++){
+			    for (y=0; y<height; y++){
+			      // Negativo
+			      setPixel(imageData, x, y, 255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y), 255);
+					}
+			 }
+		}
+
+		// Gray Scale
+			function realizar_Gray_Scale(){
+				for (x=0; x<width; x++){
+					 for (y=0; y<height; y++){
+				      prom = (getRed(imageData2,x,y) + getGreen(imageData2,x,y) + getBlue(imageData2,x,y))/3;
+				      setPixel(imageData2, x, y, prom ,prom ,prom , 255);
+						}
+				}
+			 }
+      // Sepia
+			function realizar_Sepia(){
+				for (x=0; x<width; x++){
+		 	    for (y=0; y<height; y++){
+				      red = getRed(imageData3,x,y);
+				      green = getGreen(imageData3,x,y);
+				      blue = getBlue(imageData3,x,y);
+
+				      sepiaR = Math.floor(0.393*red + 0.769*green+ 0.189*blue);
+				      sepiaG = Math.floor(0.349*red + 0.686*green + 0.168*blue);
+				      sepiaB = Math.floor(0.272*red + 0.534*green + 0.131*blue);
+				      setPixel(imageData3, x, y, sepiaR,sepiaG,sepiaB, 255);
+					}
+			 	}
+			 }
+
+	      // Binarizacion
+
+			function realizar_Binarizacion(){
+				for (x=0; x<width; x++){
+		 	    for (y=0; y<height; y++){
+			      prom2 = (getRed(imageData4,x,y) + getGreen(imageData4,x,y) + getBlue(imageData4,x,y))/3;
+			      if (prom2 < 128){// el 128 deberia se una variable editable por el usuario el cual maneje el rango de cambio
+			        prom2 = 0;
+			      }
+			      else {
+			        prom2 = 255;
+			      }
+			      setPixel(imageData4, x, y, prom2 ,prom2 ,prom2 , 255);
+			   }
+	  		}
+			}
+
+	/*
+	    // Brillo
+	    red = getRed(imageData3,x,y);
+	    green = getGreen(imageData3,x,y);
+	    blue = getBlue(imageData3,x,y);
+
+	    var  brillo = 40;
+	      red+= brillo;
+	      green+= brillo;
+	      blue+= brillo;
+	      if  ((red > 255) or (red < 0)){
+	        if (red >255){
+	          red=255;
+	        }
+	        else {
+	          red=0;
+	        }
+	      }
+	      if  ((green > 255) or (green < 0)){
+	        if (green >255){
+	          green=255;
+	        }
+	        else {
+	          green=0;
+	        }
+	      }
+
+	      if  ((blue > 255) or (blue < 0)){
+	        if (blue >255){
+	          blue=255;
+	        }
+	        else {
+	          blue=0;
+	        }
+	      }
+	*/
+
+	  ctx2.putImageData(imageData,0, 0);
+	}
 
 
-function setPixel(imageData, x, y, r, g, b, a){
-	index = (x + y * imageData.width) * 4;
-	imageData.data[index+0] = r;
-	imageData.data[index+1] = g;
-	imageData.data[index+2] = b;
-	imageData.data[index+3] = a;
-}
+	// Get de los colores
+	function getRed(imagedata, x, y){
+	  index = (x + y * imageData.width)*4;
+	  return imageData.data[index+0];
+	}
+
+	function getGreen(imagedata, x, y){
+	  index = (x + y * imageData.width)*4;
+	  return imageData.data[index+1];
+	}
+
+	function getBlue(imagedata, x, y){
+	  index = (x + y * imageData.width)*4;
+	  return imageData.data[index+2];
+	}
+
+
+	function setPixel(imageData, x, y, r, g, b, a){
+		index = (x + y * imageData.width) * 4;
+		imageData.data[index+0] = r;
+		imageData.data[index+1] = g;
+		imageData.data[index+2] = b;
+		imageData.data[index+3] = a;
+	}
