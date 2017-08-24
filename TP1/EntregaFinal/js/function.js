f3d2aa250df70f73caee65307d683be823d2c380
elmuddy <fn.rosenfeld@gmail.com>
2017-08-24 19:39:42 -0300

    TPE 1 - Aplicar Filtro Estatico
    Aplica un filtro al dar un click

--- TP1/EntregaFinal/js/function.js
+++ TP1/EntregaFinal/js/function.js
@@ -1,4 +1,21 @@
 
+
+
+
+var image1 = new Image();
+image1.src = "imagen1.jpg"; // asigna la direccion de donde cargar la imagen
+var ctx = document.getElementById("canvas").getContext("2d");
+var ctx2 = document.getElementById("canvas2").getContext("2d");
+
+image1.onload = function(){  // realiza la funcion despues de que cargo la imagen y con esto evita error
+  myDrawImageMethod(this);
+}
+  function myDrawImageMethod(image){
+    ctx.drawImage(image,0,0 ,canvas.width,canvas.height);
+    imageData =  ctx.getImageData(0,0, canvas.width,canvas.width);
+}
+
+
 $(function() {
     $('#file-input').change(function(e) {
         var file = e.target.files[0],
@@ -15,56 +32,46 @@ $(function() {
 
     function fileOnload(e) {
         var $img = $('<img>', { src: e.target.result });
-        var canvas = $('#canvas')[0];
-        var context = canvas.getContext('2d');
-
         $img.on('load', function() { // Te odio a vos y a esta linea que cambio en jquery-3
-            context.drawImage(this, 0, 0);
+            myDrawImageMethod(this); // modifique esta parte porque no andaban los filtros mas adelante
         });
     }
+
+    $('#Aplicar_Filtro_X').on('click', function() {
+       imageDataX =  ctx.getImageData(0,0, canvas.width,canvas.width);
+       realizar_Negativo(imageDataX);
+    });
+
 });
 
-	  var ctx = document.getElementById("canvas").getContext("2d");
-	  var width = 500;
-	  var height =300;
-		var ctx2 = document.getElementById("canvas2").getContext("2d");
 
-
-	var image1 = new Image();
-	image1.src = "imagen1.jpg"; // asigna la direccion de donde cargar la imagen
-
-	image1.onload = function(){  // realiza la funcion despues de que cargo la imagen y con esto evita error
-	  myDrawImageMethod(this);
-	}
-
-		function myDrawImageMethod(image){
-	  ctx.drawImage(image,0,0 ,canvas.width,canvas.height);
-	  imageData =  ctx.getImageData(0,0, canvas.width,canvas.width);
 
 		// Aca podria poner un case que me de la opcion de que filtro ponerle
 
-			function realizar_Negativo(){
-			 for (x=0; x<width; x++){
-			    for (y=0; y<height; y++){
+			function realizar_Negativo(imageData1){
+			 for (x=0; x<imageData1.width; x++){
+			    for (y=0; y<imageData1.height; y++){
 			      // Negativo
-			      setPixel(imageData, x, y, 255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y), 255);
+			      setPixel(imageData1, x, y, 255-getRed(imageData1,x,y),255-getGreen(imageData1,x,y),255-getBlue(imageData1,x,y), 255);
 					}
 			 }
+       ctx2.putImageData(imageData1,0, 0);
 		}
 
 		// Gray Scale
-			function realizar_Gray_Scale(){
-				for (x=0; x<width; x++){
-					 for (y=0; y<height; y++){
+			function realizar_Gray_Scale(imageData2){
+				for (x=0; x<ctx.width; x++){
+					 for (y=0; y<ctx.height; y++){
 				      prom = (getRed(imageData2,x,y) + getGreen(imageData2,x,y) + getBlue(imageData2,x,y))/3;
 				      setPixel(imageData2, x, y, prom ,prom ,prom , 255);
 						}
 				}
+        ctx2.putImageData(imageData2,0, 0);
 			 }
       // Sepia
-			function realizar_Sepia(){
-				for (x=0; x<width; x++){
-		 	    for (y=0; y<height; y++){
+			function realizar_Sepia(imageData3){
+				for (x=0; x<ctx.width; x++){
+		 	    for (y=0; y<ctx.height; y++){
 				      red = getRed(imageData3,x,y);
 				      green = getGreen(imageData3,x,y);
 				      blue = getBlue(imageData3,x,y);
@@ -75,13 +82,14 @@ $(function() {
 				      setPixel(imageData3, x, y, sepiaR,sepiaG,sepiaB, 255);
 					}
 			 	}
+        ctx2.putImageData(imageData3,0, 0);
 			 }
 
 	      // Binarizacion
 
-			function realizar_Binarizacion(){
-				for (x=0; x<width; x++){
-		 	    for (y=0; y<height; y++){
+			function realizar_Binarizacion(imageData4){
+        for (x=0; x<ctx.width; x++){
+           for (y=0; y<ctx.height; y++){
 			      prom2 = (getRed(imageData4,x,y) + getGreen(imageData4,x,y) + getBlue(imageData4,x,y))/3;
 			      if (prom2 < 128){// el 128 deberia se una variable editable por el usuario el cual maneje el rango de cambio
 			        prom2 = 0;
@@ -92,6 +100,7 @@ $(function() {
 			      setPixel(imageData4, x, y, prom2 ,prom2 ,prom2 , 255);
 			   }
 	  		}
+        ctx2.putImageData(imageData4,0, 0);
 			}
 
 	/*
@@ -130,11 +139,6 @@ $(function() {
 	        }
 	      }
 	*/
-
-	  ctx2.putImageData(imageData,0, 0);
-	}
-
-
 	// Get de los colores
 	function getRed(imagedata, x, y){
 	  index = (x + y * imageData.width)*4;
