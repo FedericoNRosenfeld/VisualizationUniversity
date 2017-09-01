e6199651a6fe77521f1db48ecbb5b5cdacbce224
elmuddy <fn.rosenfeld@gmail.com>
2017-09-01 16:22:03 -0300

    TPE 2 - Creacion de multiples fichas por parametro

--- TP2/EntregaFinal/js/function.js
+++ TP2/EntregaFinal/js/function.js
@@ -4,22 +4,37 @@ var ctx = canvas.getContext("2d");
 
  ///////////////CREACION DE UNA CLASE FICHA CON APLCIACION DE UN METODO
 
- function Ficha(paramPosX, paramPosY,color){
-
+ function Ficha(id,paramPosX, paramPosY,color,tamanio){
+    this.id = id;
  		this.Color = color;
  		this.posX =paramPosX;
  		this.posY =paramPosY;
- 		this.posfX = 50;
- 		this.posfY =10;
+ 		this.largoX = tamanio;
+ 		this.altoY =10;
 
  	}
+
+  function generadorFichas(cantidad,posX,posY){
+    var cX = posX;
+    var cY = posY;
+    var color = "";
+    var tamanio = cantidad*20;
+    for (i = 0; i < cantidad ;i++){
+      var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+      var fichaX = new Ficha(cantidad,cX,cY,color,tamanio);
+      dibujarFicha(fichaX);
+      cY-= 12;
+      tamanio-= 10;
+      cX+=5;
+    }
+  }
 
   /////////////// DIBUJAR  UNA  FICHA EN EL CANVAS
 
 function dibujarFicha(fichaX){
 	ctx.fillStyle = fichaX.Color;
 	ctx.beginPath();
-	ctx.fillRect(fichaX.posX,fichaX.posY,fichaX.posfX,fichaX.posfY);
+	ctx.fillRect(fichaX.posX,fichaX.posY,fichaX.largoX,fichaX.altoY);
 	ctx.fill();
 	ctx.closePath();
 	}
@@ -28,9 +43,7 @@ function dibujarFicha(fichaX){
 function mouseDown(e) {
   var cX = e.clientX;
   var cY = e.clientY;
-  var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-  var fichaX = new Ficha(cX,cY,color);
-  dibujarFicha(fichaX);
+  generadorFichas(3,cX,cY);
 }
 
 document.getElementById("canvas").addEventListener("click",mouseDown);
