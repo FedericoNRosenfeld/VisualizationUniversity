c0ce17490afb17ecc8f5d79a91f445d71b875e73
elmuddy <fn.rosenfeld@gmail.com>
2017-09-13 17:16:15 -0300

    TPE 2- Tablero Funcionando con selector de discos
    falta el cartel de GANASTE

--- TP2/EntregaFinal/js/function.js
+++ TP2/EntregaFinal/js/function.js
@@ -2,7 +2,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var oldX,oldY; //almacena las coordenas X,Y antes de realizar un desplazamiento
-var tablerolargo,tableroalto;
+var tablerolargo,tableroalto; // definidos globales en caso de necesitarlos par ael canvas mas adelante
  ///////////////CREACION DE UNA CLASE FICHA CON APLCIACION DE UN METODO
  ////// Ficha
  function Ficha(id,paramPosX, paramPosY,color,tamanio){
@@ -18,7 +18,7 @@ var tablerolargo,tableroalto;
 
   /////////////// CREACION DEL TABLERO CON APLCIACION DE UN METODO
 
-  var fichas = []; // arreglo donde se guardarian las fichas que no estan en ninguna columna
+  var mano = []; // arreglo donde se guardarian las fichas que no estan en ninguna columna
 
   function Columna(id,posX, posY,alto){
      this.id = id;
@@ -59,7 +59,7 @@ var tablerolargo,tableroalto;
       this.columnas = [];         // un arreglo de columnas
        tablerolargo = cantPiezas * 70;
        tableroalto = cantPiezas * 20;
-       canvas.width = tablerolargo * 2 ;
+       canvas.width = tablerolargo * 1.25 ;
        canvas.height = tableroalto *2.5 ;
       var posX = (canvas.width/2)-(tablerolargo/2);
       var posY = canvas.height - 22;
@@ -76,18 +76,19 @@ var tablerolargo,tableroalto;
   /////////////// METODOS DE LA COLUMNA
 
     Columna.prototype.apilarFicha = function(){ // Retira la ficha tope de la pila auxiliar y la agrega en la columna
-    fichaNueva = fichas.pop();
+    fichaNueva = mano.pop();
     //alert("fichanueva id="+fichaNueva.id+ " fichanueva posX="+ fichaNueva.posX+" fichanueva posY="+fichaNueva.posY );
 
     if (this.colFichas.length >0){
       fichaAnterior= this.colFichas[this.colFichas.length-1];
-    fichaNueva.posX = fichaAnterior.posX ;
+    fichaNueva.posX = fichaAnterior.posX + ( fichaNueva.id - fichaAnterior.id)*5; //  para que el disco mantenga su centro respecto a su tamaño en comparacion a la de abajo
  		fichaNueva.posY = fichaAnterior.posY - 12 ;
     //alert("fichanueva id="+fichaNueva.id+ " fichanueva posX="+ fichaNueva.posX+" fichanueva posY="+fichaNueva.posY );
     }
     else {
-      fichaNueva.posX = this.posicionX - 20 ;
-      fichaNueva.posY = this.posicionY - 12 ;
+      fichaNueva.posX = this.posicionX  - fichaNueva.largoX/2  ;
+      //this.posicionX - this.posicionX
+      fichaNueva.posY = this.posicionY - 12;
     }
 
 		this.colFichas.push(fichaNueva);
@@ -95,13 +96,13 @@ var tablerolargo,tableroalto;
 	}
 
 	Columna.prototype.desapilarFicha = function(){ // Retira la ficha tope de la columna y la agrega en la pila auxiliar
-		fichas.push(this.colFichas.pop());
+		mano.push(this.colFichas.pop());
 	}
 
 
 	Columna.prototype.validarFichaYLugar = function(){
 	// Chequea si es valido el movimiento de apliar en la columna la ficha que s eencuentra en el arreglo aux
-		var fichaMano = fichas[0];
+		var fichaMano = mano[0];
 		var fichaColumna = this.colFichas;
     if (fichaColumna.length > 0){
     		if (fichaMano.id > fichaColumna[fichaColumna.length-1].id){ // el id mas chico hace la ficha mas grande
@@ -114,20 +115,6 @@ var tablerolargo,tableroalto;
      return true;
 	}
 
-
-
-/*
-	function colocarFicha(columnaAnt,columnaSig){
-		// Al tener ya la ficha en el Arreglo Aux , se le pregunta a la columna seleccionada si es valida la insercion
-		if (columnaSig.validarFichaYLugar()){
-			columnaSig.apilarFicha();
-		}
-		else{
-			columnaAnt.apilarFicha();
-		}
-
-	}
-*/
   /////////////// DIBUJAR EN EL CANVAS
 
   Ficha.prototype.dibujarFicha = function(){
@@ -162,17 +149,14 @@ var tablerolargo,tableroalto;
 
   	}
 
-
-
-
   Tablero.prototype.dibujartablero = function(){
     	color = "#8B4513";
       this.piso.dibujarPiso(color);
       for (i = 0;i < this.columnas.length; i++){
       this.columnas[i].dibujarColumna(color);
       }
-      for (i = 0;i < fichas.length; i++){
-          fichas[i].dibujarFicha();
+      for (i = 0;i < mano.length; i++){
+          mano[i].dibujarFicha();
       }
   	}
 
@@ -183,66 +167,79 @@ var tablerolargo,tableroalto;
     }
 
 
+
+function festejo(){
+  ctx.clearRect(0, 0,canvas.width,canvas.height);
+}
+
+function verSiGano(){
+  // como esta es la condicion de victoria especificada la dejo en una funcion separada ya que luego puede ser modificado a distintas formas de ganar
+  if ((t.columnas[0].colFichas.length == 0) &&
+      (t.columnas[1].colFichas.length == 0) &&
+      (t.columnas[2].colFichas.length > 0)  &&
+      (mano.length == 0)){
+    alert("ganaste");
+    festejo();
+  }
+}
+
 /////////////////////////////////////////// EVENTOS DE MOUSE
 
 
-function getMousePos(canvas, evt) { // posicion del mouse en el canvas
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
-
-
+    function getMousePos(canvas, evt) { // posicion del mouse en el canvas
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    }
 
     function mousedown(e) {
       pos = getMousePos(canvas, e); // para sacar la posicion exacta del mouse en el canvas
       //se capturan coordenas del mouse
       mouseX = pos.x;
       mouseY = pos.y;
+      //analizamos por cada columna para ver si fue seleccionada
       for (var i=0; i< t.columnas.length; i++){
           columnaActual = t.columnas[i]
-      if ( (mouseX>columnaActual.posicionX-50) && (mouseX < (columnaActual.posicionX + columnaActual.ancho+50)) &&
-           (mouseY<columnaActual.posicionY) && (mouseY > (columnaActual.posicionY + columnaActual.alto) )
-         ){
-           if (fichas.length >0){
-             fich = fichas[0];
-           }
-          else {
-            fich = undefined;
-          }
-            if (fich != undefined){
-              // alert("ficha en Mano "+ fich.id);
-              if (columnaActual.validarFichaYLugar()){
-                    //alert("Esta por apilar");
-                    columnaActual.apilarFicha();
+          if ( (mouseX>columnaActual.posicionX-50) && (mouseX < (columnaActual.posicionX + columnaActual.ancho+50)) &&
+               (mouseY<columnaActual.posicionY) && (mouseY > (columnaActual.posicionY + columnaActual.alto) )
+             ){
+               if (mano.length >0){
+                 fich = mano[0];
+               }
+              else {
+                fich = undefined;
               }
-            }
-            else{
-
-            //se controla en todas las fichas de esa columna
-            for (var j=0; j<columnaActual.colFichas.length; j++) {
-            //se determina si se presiono el mouse encima de alguna ficha
-            fichaj = columnaActual.colFichas[j]
-            if ( (mouseX>fichaj.posX) &&
-                 (mouseX < (fichaj.posX + fichaj.largoX)) &&
-                 (mouseY>fichaj.posY) &&
-                 (mouseY < (fichaj.posY + fichaj.altoY) ) && (fichaj == columnaActual.colFichas[columnaActual.colFichas.length-1] )
-               ){
-                  // coordenas X,Y donde se hizo clic
-                  oldX = mouseX;
-                  oldY = mouseY;
-                  columnaActual.desapilarFicha();
-                  setInterval(paint, 50);
-                  break;
-
+              //verificamos si nuestro puntero se encuentra vacio
+                if (fich != undefined){
+                  // alert("ficha en Mano "+ fich.id);
+                  if (columnaActual.validarFichaYLugar()){
+                        //alert("Esta por apilar");
+                        columnaActual.apilarFicha();
+                  }
                 }
-            }/// cierra el for de las fichas de la columna
-          }/// cierra el else
-        } // cierra el if de la columna seleccionada
-      } // cierra el for que cicla entre las columnas existentes
-      setInterval(paint, 50);
+                else{
+                //se controla en todas las fichas de esa columna
+                for (var j=0; j<columnaActual.colFichas.length; j++) {
+                fichaj = columnaActual.colFichas[j]
+                //se determina si se presiono el mouse encima de alguna ficha
+                if ( (mouseX>fichaj.posX) &&
+                     (mouseX < (fichaj.posX + fichaj.largoX)) &&
+                     (mouseY>fichaj.posY) &&
+                     (mouseY < (fichaj.posY + fichaj.altoY) ) && (fichaj == columnaActual.colFichas[columnaActual.colFichas.length-1] )
+                   ){
+                     // en caso de seleccionar una ficha y que esta sea tope de columna , sacamos dicho tope y lo agregamos a fichas[] que seria el equivalente a la mano
+                      columnaActual.desapilarFicha();
+                      setInterval(paint, 50);
+                      break;
+                    }
+                }/// cierra el for de las fichas de la columna
+              }/// cierra el else
+            } // cierra el if de la columna seleccionada
+          } // cierra el for que cicla entre las columnas existentes
+          setInterval(paint, 50);
+          verSiGano();
 
   }// cierra el metodo
 
@@ -251,38 +248,31 @@ function getMousePos(canvas, evt) { // posicion del mouse en el canvas
     mouseX = pos.x;
     mouseY = pos.y;
   //si existe una Ficha seleccionada
-  if (fichas.length > 0){
-    fichaSelected = fichas[0];
+  if (mano.length > 0){
+    fichaSelected = mano[0];
   }
   else {
     fichaSelected = undefined;
   }
   if (fichaSelected != undefined) {
-      //se calcula la distancia del dezplazamiento
-      var dx =  mouseX - oldX ;
-      var dy =  mouseY- oldY ;
-      //se asignan nuevos valores
-      oldX = mouseX;
-      oldY = mouseY;
-      //se actualiza coordenadas X,Y de la Ficha seleccionada
-      //fichas[0] = new Ficha( fichaSelected.id, fichaSelected.Color, fichaSelected.posX,  fichaSelected.posY,fichaSelected.largoX,fichaSelected.altoY );
-      fichas[0].posX = mouseX - (fichas[0].largoX/2);
-      fichas[0].posY = mouseY - (fichas[0].altoY/2);
+      mano[0].posX = mouseX - (mano[0].largoX/2);
+      mano[0].posY = mouseY - (mano[0].altoY/2);
       setInterval(paint, 50);
       }
   }
 
-  //function mouseup(e) { /// ver si es necesario
-
-   //todo el lienzo se repinta cada 50 milisegundos
-//setInterval(paint, 50);}
-
     document.getElementById("canvas").addEventListener("click",mousedown);
     document.getElementById("canvas").addEventListener("mousemove",mousemove);
-    //document.getElementById("canvas").addEventListener("mouseup",mouseup);
-
-
 
   /////////////// Llamado al tablero inicial
-  t = new Tablero(5);
+  t = new Tablero(3);
   t.dibujartablero();
+
+  /////////////// Llamado al tablero con eleccion de discos
+
+  $(function() {
+    $('#valor').change(function() {
+      t = new Tablero($(this).val());
+      t.dibujartablero();
+    });
+  });
