4723d715ee2695ef7a3674628b3df95814daede2
elmuddy <fn.rosenfeld@gmail.com>
2017-09-13 09:57:21 -0300

    TPE 2 - movimientos del mouse - desapila,mueve y explota
    Pero desapila y mueve

--- TP2/EntregaFinal/js/function.js
+++ TP2/EntregaFinal/js/function.js
@@ -88,8 +88,8 @@ var oldX,oldY; //almacena las coordenas X,Y antes de realizar un desplazamiento
 
 	Columna.prototype.validarFichaYLugar = function(){
 	// Chequea si es valido el movimiento de apliar en la columna la ficha que s eencuentra en el arreglo aux
-		var fichaMano = fichas[fichas.length-1];
-		var fichaColumna = this.colFichas[colFichas.length-1];
+		var fichaMano = fichas[0];
+		var fichaColumna = this.colFichas[this.colFichas.length-1];
 		if (fichaMano.id > fichaColumna.id){ // el id mas chico hace la ficha mas grande
 			return true;
 		}
@@ -99,11 +99,8 @@ var oldX,oldY; //almacena las coordenas X,Y antes de realizar un desplazamiento
 	}
 
 
-	function levantarFicha(columna){
-		columna.desapilarFicha();
-	}
 
-
+/*
 	function colocarFicha(columnaAnt,columnaSig){
 		// Al tener ya la ficha en el Arreglo Aux , se le pregunta a la columna seleccionada si es valida la insercion
 		if (columnaSig.validarFichaYLugar()){
@@ -114,7 +111,7 @@ var oldX,oldY; //almacena las coordenas X,Y antes de realizar un desplazamiento
 		}
 
 	}
-
+*/
   /////////////// DIBUJAR EN EL CANVAS
 
   Ficha.prototype.dibujarFicha = function(){
@@ -166,6 +163,7 @@ var oldX,oldY; //almacena las coordenas X,Y antes de realizar un desplazamiento
   function paint() {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       t.dibujartablero();
+      fichas[0].dibujarFicha;
     }
 
 
@@ -187,13 +185,22 @@ function getMousePos(canvas, evt) { // posicion del mouse en el canvas
       //se capturan coordenas del mouse
       mouseX = pos.x;
       mouseY = pos.y;
-
       for (var i=0; i< t.columnas.length; i++){
           columnaActual = t.columnas[i]
       if ( (mouseX>columnaActual.posicionX-50) && (mouseX < (columnaActual.posicionX + columnaActual.ancho+50)) &&
            (mouseY<columnaActual.posicionY) && (mouseY > (columnaActual.posicionY + columnaActual.alto) )
          ){
-            alert("pisaste columna "+ t.columnas[i].id);
+           fich = fichas[0] ;
+
+            if (fich != undefined){
+               alert("ficha en Fichas "+ fich.id);
+              if (columnaActual.validarFichaYLugar()){
+                    alert("id de la ficha en mano es = " + fichas[0].id);
+                    columnaActual.apilarFicha();
+              }
+            }
+            else{
+
             //se controla en todas las fichas de esa columna
             for (var j=0; j<columnaActual.colFichas.length; j++) {
             //se determina si se presiono el mouse encima de alguna ficha
@@ -206,11 +213,13 @@ function getMousePos(canvas, evt) { // posicion del mouse en el canvas
                   // coordenas X,Y donde se hizo clic
                   oldX = mouseX;
                   oldY = mouseY;
-                  levantarFicha(columnaActual);
-                  alert("id de la ficha = " + fichaj.id);
+                  columnaActual.desapilarFicha();
+
                   break;
+
                 }
             }/// cierra el for de las fichas de la columna
+          }/// cierra el else
         } // cierra el if de la columna seleccionada
       } // cierra el for que cicla entre las columnas existentes
   }// cierra el metodo
@@ -219,8 +228,8 @@ function getMousePos(canvas, evt) { // posicion del mouse en el canvas
     pos = getMousePos(canvas, e);
     mouseX = pos.x;
     mouseY = pos.y;
-  //si existe una FIcha seleccionada
-  fichaSelected = fichas.pop();
+  //si existe una Ficha seleccionada
+  fichaSelected = fichas[0];
   if (fichaSelected != undefined) {
       //se calcula la distancia del dezplazamiento
       var dx =  mouseX - oldX ;
@@ -229,20 +238,21 @@ function getMousePos(canvas, evt) { // posicion del mouse en el canvas
       oldX = mouseX;
       oldY = mouseY;
       //se actualiza coordenadas X,Y de la Ficha seleccionada
-      fichaVal = fichaSelected
-      fichas[0] = new Ficha( fichaVal.id, fichaVal.Color, fichaVal.posX + dx,  fichaVal.posY + dy,fichaVal.largoX,fichaVal.altoY );
+      //fichas[0] = new Ficha( fichaSelected.id, fichaSelected.Color, fichaSelected.posX,  fichaSelected.posY,fichaSelected.largoX,fichaSelected.altoY );
+      fichas[0].posX = mouseX
+      fichas[0].posY = mouseY
+      setInterval(paint, 50);
       }
   }
 
-  function mouseup(e) {
-   selectedShape = undefined;
-   //todo el lienzo se repinta cada 50 milisegundos
-   setInterval(paint, 50);
-   }
+  //function mouseup(e) { /// ver si es necesario
 
-    document.getElementById("canvas").addEventListener("mousedown",mousedown);
+   //todo el lienzo se repinta cada 50 milisegundos
+//setInterval(paint, 50);}
+
+    document.getElementById("canvas").addEventListener("click",mousedown);
     document.getElementById("canvas").addEventListener("mousemove",mousemove);
-    document.getElementById("canvas").addEventListener("mouseup",mouseup);
+    //document.getElementById("canvas").addEventListener("mouseup",mouseup);
 
 
 
