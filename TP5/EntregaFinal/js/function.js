72506e35123ba5e514768f92ce0c923cb149d568
elmuddy <fn.rosenfeld@gmail.com>
2017-11-05 16:39:20 -0300

    Entidades, empezando la consulta cb.__cal

--- TP5/EntregaFinal/js/function.js
+++ TP5/EntregaFinal/js/function.js
@@ -2,7 +2,7 @@
 var imagenes = [];
 
 /// --------------------------------------------    Authentication
-var Codebird = require("cd/codebird");
+//var Codebird = require("cd/codebird");
 // or with leading "./", if the codebird.js file is in your main folder:
 // var Codebird = require("./codebird");
 var cb = new Codebird;
@@ -10,28 +10,35 @@ cb.setConsumerKey("8Vmq8hzhPIPjYc4EAfGQ3vlke", "8h2fSU4vYhLqeJJmpRQRv9JxwtUP6jkm
 cb.setToken("163239019-rJLe5uULgm7ZVDO8yPVstjugvQRSqB6LzEtiakAV", "FnOHS2LkDQfICIBmlwia4oCsfGrSx4ohBQPOmViuToBTU");
 
 
-        //////// Application-only auth
-/*
-        cb.__call(
-            "oauth2_token",
-            {},
-            function (reply, err) {
-                var bearer_token;
-                if (err) {
-                    console.log("error response or timeout exceeded" + err.error);
-                }
-                if (reply) {
-                    bearer_token = reply.access_token;
-                }
-            }
-       );
-       */
-       /////////// Requests with app-only auth
-
        var params = {
-            q: "NYC";
+            //%23  = #
+            // q: consulta que se haria
+            q: "NYC"
         };
+// Tema de las entidades para los parametros
+// https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
 
+
+/*
+{"entities": {
+   "hashtags": [
+     {
+       "text": "hashtag"
+     }
+   ]
+}
+}
+
+{
+"extended_entities": {
+   "media":[
+      {
+        "type": "photo"
+      }
+  ]
+}
+}
+*/
         cb.__call(
             "search_tweets",
             params,
