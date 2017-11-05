d08a0f8e9ef54625c72a3345de5ea27770589943
elmuddy <fn.rosenfeld@gmail.com>
2017-11-05 18:39:37 -0300

    search_tweet 70%

--- TP5/EntregaFinal/js/function.js
+++ TP5/EntregaFinal/js/function.js
@@ -9,44 +9,45 @@ var cb = new Codebird;
 cb.setConsumerKey("8Vmq8hzhPIPjYc4EAfGQ3vlke", "8h2fSU4vYhLqeJJmpRQRv9JxwtUP6jkmBU4vgitWR6Gjt859Ov");
 cb.setToken("163239019-rJLe5uULgm7ZVDO8yPVstjugvQRSqB6LzEtiakAV", "FnOHS2LkDQfICIBmlwia4oCsfGrSx4ohBQPOmViuToBTU");
 
+ var params = {
+   // q: consulta que se haria
+      q: "NYC"
 
-       var params = {
-            //%23  = #
-            // q: consulta que se haria
-            q: "NYC"
-        };
-// Tema de las entidades para los parametros
-// https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
+  };
 
-
-/*
-{"entities": {
-   "hashtags": [
-     {
-       "text": "hashtag"
+cb.__call(
+"search_tweets",
+params,
+function (reply) {
+ for (var i = 0; i < reply.statuses.length; i++) {
+  // var pertenece = false;
+   var twitter = reply.statuses[i];
+   if(( twitter.extended_entities && twitter.extended_entities.media[0].type == "photo" ){
+     /*
+     var hasht = twitter.entities.hashtags;
+     for (var j = 0; j < hasht.length; j++) {
+         if ( hasht[i].text == params.q){
+            pertenece = true;
+         }
      }
-   ]
+     if (pertenece){ */
+       var paquete = {
+           // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
+           url: twitter.extended_entities.media[0].media_url_https,
+           //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
+           likes: twitter.favorite_count
+       }
+        imagenes.push(paquete);
+     }
+   }
+ }
+  cargarImagenes();// la encargada de mandar las imagenes a la pagina
 }
-}
-
-{
-"extended_entities": {
-   "media":[
-      {
-        "type": "photo"
-      }
-  ]
-}
-}
-*/
-        cb.__call(
-            "search_tweets",
-            params,
-            function (reply) {
-                // ...
-            }
-        );
+);
 
 
-    }
-});
+function cargarImagenes(){
+  for (var i = 0; i < imagenes.length; i++) {
+      // unir esto con los contenedores correspondientes
+  }
+}
