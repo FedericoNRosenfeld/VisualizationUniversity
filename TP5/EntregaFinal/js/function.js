627f1375b832f27bad00a82bd27bfd8d3750e857
elmuddy <fn.rosenfeld@gmail.com>
2017-11-08 16:41:34 -0300

    Pantalla de Index, el resto movido a galeria.html

--- TP5/EntregaFinal/js/function.js
+++ TP5/EntregaFinal/js/function.js
@@ -18,39 +18,37 @@ var cb = new Codebird;
 cb.setConsumerKey("8Vmq8hzhPIPjYc4EAfGQ3vlke", "8h2fSU4vYhLqeJJmpRQRv9JxwtUP6jkmBU4vgitWR6Gjt859Ov");
 cb.setToken("163239019-rJLe5uULgm7ZVDO8yPVstjugvQRSqB6LzEtiakAV", "FnOHS2LkDQfICIBmlwia4oCsfGrSx4ohBQPOmViuToBTU");
 
-consulta("perro");
+//consulta("perro");
 
 function consulta(busqueda){
   imagenes = [];
  var params = {
    //https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
-      q: "#"+busqueda, /// busqueda a realizar
-      result_type: "mixed", // tipo de busqueda mixta
-      count:50 // maximo de 50 twitters
-  }
+      q: "#"+busqueda, 			// busqueda a realizar
+	  result_type: "mixed", 	// tipo de busqueda mixta
+      count:50 					// maximo de 50 twitters
+  };
   console.log(params.q);
-
-
-cb.__call(
-"search_tweets",
-params,
-function (reply) {
- for (var i = 0; i < reply.statuses.length; i++) {
-  // var pertenece = false;
-   var twitter = reply.statuses[i];
-   if(( twitter.extended_entities && twitter.extended_entities.media[0].type == "photo" )){
-     /* var hasht = twitter.entities.hashtags; for (var j = 0; j < hasht.length; j++) {if ( hasht[i].text == params.q){pertenece = true;}} if (pertenece){ */
-       var paquete = {
-           // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
-           url: twitter.extended_entities.media[0].media_url_https,
-           //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
-           likes: twitter.favorite_count
-       }
-        imagenes.push(paquete);
-     }
-   }
-  cargarImagenes();// la encargada de mandar las imagenes a la pagina
-}
+	cb.__call(
+	"search_tweets",
+	params,
+	function (reply) {
+	 for (var i = 0; i < reply.statuses.length; i++) {
+	  // var pertenece = false;
+	   var twitter = reply.statuses[i];
+	   if(( twitter.extended_entities && twitter.extended_entities.media[0].type == "photo" )){
+		 /* var hasht = twitter.entities.hashtags; for (var j = 0; j < hasht.length; j++) {if ( hasht[i].text == params.q){pertenece = true;}} if (pertenece){ */
+		   var paquete = {
+			   // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
+			   url: twitter.extended_entities.media[0].media_url_https,
+			   //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
+			   likes: twitter.favorite_count
+		   }
+			imagenes.push(paquete);
+		 }
+	   }
+	  cargarImagenes();// la encargada de mandar las imagenes a la pagina
+	}
 
 );
 }
