7fba8c326a3cfb7c495e070b74ea1453104c34ef
Fabian Martino <fabian.martino.m@gmail.com>
2017-11-15 17:07:48 -0300

    vista 1(grilla) funcionando

--- TP5/EntregaFinal/js/function.js
+++ TP5/EntregaFinal/js/function.js
@@ -1,12 +1,11 @@
-
- document.body.style.background = "url('css/images/background_t.png')";
+$( document ).ready(function() {
 /// --------------------------------------------    Authentication
 //var Codebird = require('cd/codebird');
 // or with leading "./", if the codebird.js file is in your main folder:
 // var Codebird = require("./codebird");
 //CONSUMER ALT "ZjATE7n0u6cve4juuPiyitw9O", "32DuRj57NXTKp7pOMCw4c7Ubsee57qffr2jrk8vAmjHwwwLg4b"
 //TOKEN ALT "926493527856566272-DW8mbl8NYupY9WY4ce9pUuCqALoKpPL", "m2Anmygd0uJ1tjTlU5WC8JCVH6S6R9DlTugfEQbwb9z8v"
-
+var vistaActiva = "grilla";
 var cambio_tipo = true;
 var imagenes = [];
 /// --------------------------------------------    Authentication
@@ -17,12 +16,18 @@ var cb = new Codebird;
 cb.setConsumerKey("8Vmq8hzhPIPjYc4EAfGQ3vlke", "8h2fSU4vYhLqeJJmpRQRv9JxwtUP6jkmBU4vgitWR6Gjt859Ov");
 cb.setToken("163239019-rJLe5uULgm7ZVDO8yPVstjugvQRSqB6LzEtiakAV", "FnOHS2LkDQfICIBmlwia4oCsfGrSx4ohBQPOmViuToBTU");
 
-//cb.setProxy("https://cb-proxy.herokuapp.com/"); // proxy que paso Axel en caso que no ande CB
+//cb.setProxy("https://cb-proxy.herokuapp.com/"); // proxy que paso Axel
 
-function consulta(busqueda,tipo){
+
+function consultar(busqueda,tipo){
+  document.getElementById("paginaInicio").style.display = "none";
+  document.getElementById("paginaGaleria").style.display = "block"
+  document.getElementById("grilla").style.backgroundColor = "lightblue";
+  document.getElementById("exposicion").style.backgroundColor = "white";
+  document.getElementById("presentacion").style.backgroundColor = "white";
   var consulta; // para chequear si no arranca con un #
   consulta = busqueda.substring(0,1);
-  if  (consulta <> "#"){
+  if  (consulta != "#"){
     consulta = "#"+ busqueda;
   }
   else {
@@ -33,7 +38,7 @@ function consulta(busqueda,tipo){
 
       q: consulta, 			// busqueda a realizar , hacer un chequeo si lo que viene no posee ya un #
       result_type: tipo, 	// mixta era el dafault pero no traia todos, sino que traia los que tenia en comun
-      count:50 					// maximo de 50 twitters
+      count:50, 					// maximo de 50 twitters
   };
   console.log(params.q);
 	cb.__call(
@@ -48,9 +53,9 @@ function consulta(busqueda,tipo){
 			   // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
 			   url: twitter.extended_entities.media[0].media_url_https,
 			   //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
-			   likes: twitter.favorite_count
+			   likes: twitter.favorite_count,
 			   // Nombre del emisor del twit
-			   usuario: twiter.user[0].name;
+			   usuario: twitter.user.name
 		   }
 		   // armar un contains para evitar imagenes repetidas (re-twitts)
       if (! existeImagen(paquete)){
@@ -61,14 +66,12 @@ function consulta(busqueda,tipo){
 	   }
 	   if (cambio_tipo){
           cambio_tipo = false; //asi evita entrar en el if la segunda vuelta
-          consulta(busqueda,"recent");
+          consultar(busqueda,"recent");
 	   }
 	   else{
         cambiar_tipo = true; // para que quede seteado en default otra vez
-        cargarImagenes();// la encargada de mandar las imagenes a la pagina
-
+        cargarvista();
 	   }
-
 	}
 
 );
@@ -87,62 +90,138 @@ function existeImagen(paquete){
 
 }
 
-
-function cargarImagenes(){
-  alert( imagenes.length);
-  var img = document.getElementById("galeria");
+//-----------------------metodos de carga de galerias----------------------------//
+function cargargrilla(){
+  var img = document.getElementById("grill");
   while (img.hasChildNodes()) {
       img.removeChild(img.firstChild);
   }
-  var divs = document.createElement('div');
-  divs.className = "row";
-  divs.id = 'grill'
+
   for (var i = 0; i < imagenes.length; i++) {
     console.log(imagenes[i]);
     var divtamano = document.createElement('div');
-    divtamano.className = "col-md-3";
-    var divclase = document.createElement('div');
-    divclase.className = "imagenesGaleria";
+    divtamano.className = "col-sm-3 imagenesGaleria";
+    var divImages = document.createElement('div');
+    //divImages.className = "imagenesGaleria";
     var images = document.createElement('img');
     images.src = imagenes[i].url;
     images.id = i;
     images.className = "img-responsive";
-    divclase.appendChild(images);
-    divtamano.appendChild(divclase);
-    divs.appendChild(divtamano);
+    var divDatos = document.createElement('div');
+    divDatos.className = "row";
 
+    var likes = document.createElement('div');
+    likes.className = "text-right col-sm-6 likes";
+    var figura = document.createElement('i');
+    figura.className = "fa fa-heart";
+    var texto = document.createTextNode(imagenes[i].likes);
+    var divUser = document.createElement('div');
+    divUser.className = "text-left col-sm-6 user";
+    var p = document.createElement('p');
+    var nombre = document.createTextNode("@"+imagenes[i].usuario);
+
+    p.appendChild(nombre);
+    divUser.appendChild(p);
+
+
+    figura.appendChild(texto);
+    likes.appendChild(figura);
+
+    divDatos.appendChild(divUser);
+    divDatos.appendChild(likes);
+
+    divImages.appendChild(images);
+    divtamano.appendChild(divImages);
+    divtamano.appendChild(divDatos);
+    document.getElementById('grill').appendChild(divtamano);
   }
-  document.getElementById('galeria').appendChild(divs);
 }
 
+//-------------------------Cargado de las vistas----------------------------------//
+
+var cargarvista = function() {
+  var pagina = "";
+  switch (vistaActiva) {
+    case "grilla":
+      pagina = "vista1";
+      break;
+    case "exposicion":
+      pagina = "vista2";
+      break;
+    case "presentacion":
+      pagina = "vista3";
+      break;
+  }
+  $.ajax({
+    url: pagina+".html",
+    method:"GET",
+    dataType:"html",
+    success: function(textoCargado, status){
+      $("#contenido").html(textoCargado);
+      switch (vistaActiva) {
+        case "grilla":
+          cargargrilla();
+          break;
+        case "exposicion":
+          pagina = "vista2";
+          break;
+        case "presentacion":
+          pagina = "vista3";
+          break;
+      }
+
+    }
+  });
+};
+//-------------------------pagina de inicio----------------------------------//
+
+
+$(document).on("click", "#primeraBusqueda", function(e){
+  e.preventDefault();
+  document.body.style.background = "url('css/images/background_t.png')";
+  var busqueda = document.getElementById("primerFormulario").buscar.value;
+  console.log(busqueda);
+  consultar(busqueda,"popular");
+});
+
+
+//-------------------------Busqueda--------------------------------------------//
 $('#hash').submit(function(e){
   e.preventDefault();
-  document.getElementById("grilla").style.backgroundColor = "lightblue";
   var busqueda = document.getElementById("hash").buscar.value;
   imagenes = [];
-  consulta(busqueda,"popular");
-  $("body").load("vista1");
+  consultar(busqueda,"popular");
+  cargarvista();
+  document.getElementById("grilla").style.backgroundColor = "lightblue";
 });
+
+//-------------------------Cambio de galerias--------------------------------------------//
+
 $('#grilla').click(function(e){
   e.preventDefault();
-  cargarpagina("vista1.html");
+  vistaActiva = "grilla";
+  cargarvista();
   document.getElementById("grilla").style.backgroundColor = "lightblue";
   document.getElementById("exposicion").style.backgroundColor = "white";
   document.getElementById("presentacion").style.backgroundColor = "white";
 });
+
 $('#exposicion').click(function(e){
   e.preventDefault();
-  cargarpagina("vista2.html");
+  vistaActiva = "exposicion";
+  cargarvista();
   document.getElementById("grilla").style.backgroundColor = "white";
   document.getElementById("exposicion").style.backgroundColor = "lightblue";
   document.getElementById("presentacion").style.backgroundColor = "white";
 });
+
 $('#presentacion').click(function(e){
-  cargarpagina("vista3.html");
+  e.preventDefault();
+  vistaActiva = "presentacion";
+  cargarvista();
   document.getElementById("grilla").style.backgroundColor = "white";
   document.getElementById("exposicion").style.backgroundColor = "white";
   document.getElementById("presentacion").style.backgroundColor = "lightblue";
 });
-function cargarpagina(pagina){
-  location.href = pagina;
-}
+
+});
